2118d9c49cf041cadecb99a09d9601b27f8a8081
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-11-09 19:50:22 -0500

    footer and navbar added

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,25 +1,25 @@
 import Head from "next/head"
 import { Box } from "@chakra-ui/react"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
 
-const Layout = ({ children }) => {
-  return (
+const Layout = ({ children }) => (
     <>
         <Head>
-            <title>Real Estate</title>
+            <title>Figgs Real Estate</title>
         </Head>
         <Box maxWidth='1280px' m='auto'>
             <header>
-                Navbar
+                <Navbar />
             </header>
             <main>
                 {children}
             </main>
             <footer>
-                Footer
+                <Footer />
             </footer>
         </Box>
     </>
-  )
-}
+)
 
 export default Layout
