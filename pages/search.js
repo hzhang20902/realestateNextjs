93cf82d9081794fbe2e13d09b41dbcd4e08ae196
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-11-11 00:16:52 -0500

    search query data fetch functional

--- pages/search.js
+++ pages/search.js
@@ -6,8 +6,9 @@ import { BsFilter } from 'react-icons/bs'
 import SearchFilters from '../components/SearchFilters'
 import Property from '../components/Property'
 import noResult from '../assets/images/noresult.svg'
+import { fetchApi, baseUrl } from '../utils/fetchApi'
 
-const Search = () => {
+const Search = ({ properties }) => {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
 
@@ -33,9 +34,9 @@ const Search = () => {
             Properties {router.query.purpose}
         </Text>
         <Flex flexWrap='wrap'>
-            {[].map((property) => <Property property={property} key={property.id} />)}
+            {properties.map((property) => <Property property={property} key={property.id} />)}
         </Flex>
-        {[].length === 0 && (
+        {properties.length === 0 && (
             <Flex justifyContent='center' alignItems='center' flexDirection='column' marginTop='5' marginBottom='5'>
                 <Image alt='no result' src={noResult}/>
                 <Text fontSize='2xl' marginTop='3'>No Results Found</Text>
@@ -46,3 +47,31 @@ const Search = () => {
 }
 
 export default Search
+//notice the difference between getServerSideProps below and getStaticPorps
+// static: fetch data at build time, so its already there before the user
+// serverside: dynamic data fetch at time of user request
+
+export async function getServerSideProps({ query }) {
+    const purpose = query.purpose || 'for-rent';
+    const rentFrequency = query.rentFrequency || 'yearly';
+    const minPrice = query.minPrice || '0';
+    const maxPrice = query.maxPrice || '1000000';
+    const roomsMin = query.roomsMin || '0';
+    const bathsMin = query.bathsMin || '0';
+    const sort = query.sort || 'price-desc';
+    const areaMax = query.areaMax || '35000';
+    const locationExternalIDs = query.locationExternalIDs || '5002';
+    const categoryExternalID = query.categoryExternalID || '4';
+
+    //the url in literal CANNOT be separated by line
+    const data = await fetchApi(
+        `${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&minPrice=${minPrice}&maxPrice=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`
+    )
+  
+    return {
+      props: {
+        properties: data?.hits,
+      }
+    }
+  }
+
