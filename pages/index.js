ce014921e2fb605e8d78248c681758f9bc8e28fe
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-11-11 02:16:56 -0500

    fixed home page button link query

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home({ propertiesForRent, propertiesForSale }) {
         desc1="Explore Apartments, Villas, Homes"
         desc2="and more"
         buttonText="Explore Renting"
-        linkName="/search/purpose=for-rent"
+        linkName="/search?purpose=for-rent"
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4' 
         />
         <Flex flexWrap="wrap">
@@ -42,7 +42,7 @@ export default function Home({ propertiesForRent, propertiesForSale }) {
         desc1="Explore Apartments, Villas, Homes"
         desc2="and more"
         buttonText="Explore Buying"
-        linkName="/search/purpose=for-sale"
+        linkName="/search?purpose=for-sale"
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008' 
         />
          <Flex flexWrap="wrap">
